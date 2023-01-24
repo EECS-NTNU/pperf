66243da73a519b10a74e9ad2b797c4730e325ac5
Björn Gottschall <github.mail@bgottschall.de>
2023-01-24 14:19:29 +0100

    * create cache by default does not build with sources

--- python/createCache.py
+++ python/createCache.py
@@ -9,7 +9,7 @@ parser.add_argument("-n", "--name", help="choose a different name for this execu
 parser.add_argument("-d", "--dynmap", help="provide dynamic branch informations as csv", default=None)
 parser.add_argument("-s", "--search-path", help="add search path for source code files", action="append", default=[])
 parser.add_argument("--unwind-inline", help="unwind inlined functions", action="store_true", default=False)
-parser.add_argument("--no-source", help="do not include source code", action="store_true", default=False)
+parser.add_argument("--with-sources", help="do not include source code", action="store_true", default=False)
 parser.add_argument("--no-basic-block-reconstruction", help="do not try to reconstruct basic blocks", action="store_true", default=False)
 args = parser.parse_args()
 
@@ -23,4 +23,4 @@ cache = profileLib.elfCache();
 if cache.cacheAvailable(args.elf) and not args.force:
     print(f'INFO: cache already available at {cache.getCacheFile(args.elf)}, force rebuild via --force')
 else:
-    cache.createCache(args.elf, name=args.name, sourceSearchPaths = args.search_path, dynmapfile=args.dynmap, includeSource=not args.no_source, basicblockReconstruction=not args.no_basic_block_reconstruction);
+    cache.createCache(args.elf, name=args.name, sourceSearchPaths = args.search_path, dynmapfile=args.dynmap, includeSource=args.with_sources, basicblockReconstruction=not args.no_basic_block_reconstruction);
